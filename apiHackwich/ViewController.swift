140f4d823c1014b7928db1fd660c523037f01e08
MIGUEL TAMAYO <rl237854@rlas-macair-32.rlas-116.org>
2018-10-01 12:41:48 -0500

    Added API swift

--- apiHackwich/ViewController.swift
+++ apiHackwich/ViewController.swift
@@ -2,12 +2,19 @@ import UIKit
 
 class SourcesViewController: UITableViewController {
 
+    var sources = [[String: String]]()
+    var apiKey = "679376b10aad42019ff49513ad555602"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-    }
+       self.title = "news Sources"
+        let query =  let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
+     
+        }
 
-    
+    func parse(json: JSON) {
+        
+    }
 
 }
 
